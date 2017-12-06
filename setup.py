7849d12cb6313ece9ad7dab0b61e048ce21cf8f3
Michael Howitz <mh@gocept.com>
2017-12-06 16:12:36 +0100

    Rename file extensions to match content.

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 """
 
 from setuptools import setup, find_packages
-import glob
 import os.path
 
 
@@ -44,15 +43,14 @@ Programming Language :: Python :: 2 :: Only
 """[:-1].split('\n'),
     description=__doc__.strip(),
     long_description='\n\n'.join(open(project_path(name)).read() for name in (
-        'README.txt',
-        'HACKING.txt',
-        'CHANGES.txt',
+        'README.rst',
+        'HACKING.rst',
+        'CHANGES.rst',
     )),
 
     namespace_packages=['gocept'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    data_files=[('', glob.glob(project_path('*.txt')))],
     zip_safe=False,
 )
