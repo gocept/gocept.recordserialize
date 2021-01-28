d309ea264b7ec4de6cbe84c6e7837242448097f6
Steffen Allner <sa@gocept.com>
2021-01-28 08:43:10 +0100

    Use pre-commit for linting.

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import gocept.package.sphinxconf
+
+
 _year_started = 2012
 
-import gocept.package.sphinxconf
 gocept.package.sphinxconf.set_defaults()
--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 """File writer/parser for fixed-width and character-separated files
 """
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 
 setup(
